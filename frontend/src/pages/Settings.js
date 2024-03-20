e2c6d913e61b0da8c3142929f6626d5246e0e805
Delaney H <steam.avocado@pm.me>
2024-03-20 12:06:56 -0230

    Added useState to settings page

--- frontend/src/pages/Settings.js
+++ frontend/src/pages/Settings.js
@@ -1,28 +1,40 @@
-import {Link, Routes, Route} from "react-router-dom";
-
+import { useState } from 'react';
+import ReactDOM from 'react-dom';
 
 export const Settings = () => {
+    const [darkMode, setDarkMode] = useState(false);
+    const [notifications, setNotifications] = useState(false);
+    const [unitOfMeasurement, setUnitOfMeasurement] = useState('metric');
+    const [colorBlindness, setColorBlindness] = useState('normal');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
     return (
         <div>
             <h1>Settings</h1>
             <form>
                 <h3>General</h3>
                 <label>
-                    <input type="checkbox" id="darkMode" name="darkMode" value="darkMode"/>
+                    <input type="checkbox" id="darkMode" name="darkMode" value="darkMode"
+                    onChange={() => setDarkMode(!darkMode)} />
                     Dark Mode
                 </label>
                 <br/>
 
                 <label>
-                    <input type="checkbox" id="notifications" name="notifications" value="notifications"/>
+                    <input type="checkbox" id="notifications" name="notifications" value="notifications"
+                    onChange={() => setNotifications(!notifications)} />
                     Notifications
                 </label>
                 <br/>
 
                 <label>
-                    <select id="unitOfMeasurement" name="unitOfMeasurement">
-                        <option value="imperial">Imperial</option>
+                    <select id="unitOfMeasurement" name="unitOfMeasurement"
+                    onChange={(e) => setUnitOfMeasurement(e.target.value)} >
                         <option value="metric">Metric</option>
+                        <option value="imperial">Imperial</option>
                     </select>
                     Unit of Measurement
                 </label>
@@ -32,10 +44,14 @@ export const Settings = () => {
 
                 <h4>Color Blind Filter</h4>
                 <label>
-                    <input type="radio" id="normal" name="colorBlindness" value="normal"/>No Filter
-                    <input type="radio" id="deuteranopia" name="colorBlindness" value="deuteranopia"/>Deuteranopia
-                    <input type="radio" id="protanopia" name="colorBlindness" value="protanopia"/>Protanopia
-                    <input type="radio" id="tritanopia" name="colorBlindness" value="tritanopia"/>Tritanopia
+                    <input type="radio" id="normal" name="colorBlindness" value="normal"
+                    onClick={() => setColorBlindness('normal')} />No Filter
+                    <input type="radio" id="deuteranopia" name="colorBlindness" value="deuteranopia"
+                    onClick={() => setColorBlindness('deuteranopia')} />Deuteranopia
+                    <input type="radio" id="protanopia" name="colorBlindness" value="protanopia"
+                    onClick={() => setColorBlindness('protanopia')} />Protanopia
+                    <input type="radio" id="tritanopia" name="colorBlindness" value="tritanopia"
+                    onClick={() => setColorBlindness('tritanopia')} />Tritanopia
                 </label>
             </form>
         </div>
