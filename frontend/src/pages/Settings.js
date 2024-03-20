76b847e06294430fead1a6ad92400adacfea7f4c
Delaney H <steam.avocado@pm.me>
2024-03-20 10:17:53 -0230

    Added general & accessibility section Available settings are: Dark Mode, Notifications, Unit of Measurement, and Color Blind Filter

--- frontend/src/pages/Settings.js
+++ frontend/src/pages/Settings.js
@@ -5,8 +5,39 @@ export const Settings = () => {
     return (
         <div>
             <h1>Settings</h1>
-            <p>Change your settings here</p>
-            <p>Placeholder</p>
+            <form>
+                <h3>General</h3>
+                <label>
+                    <input type="checkbox" id="darkMode" name="darkMode" value="darkMode"/>
+                    Dark Mode
+                </label>
+                <br/>
+
+                <label>
+                    <input type="checkbox" id="notifications" name="notifications" value="notifications"/>
+                    Notifications
+                </label>
+                <br/>
+
+                <label>
+                    <select id="unitOfMeasurement" name="unitOfMeasurement">
+                        <option value="imperial">Imperial</option>
+                        <option value="metric">Metric</option>
+                    </select>
+                    Unit of Measurement
+                </label>
+                <br/>
+
+                <h3>Accessibility</h3>
+
+                <h4>Color Blind Filter</h4>
+                <label>
+                    <input type="radio" id="normal" name="colorBlindness" value="normal"/>No Filter
+                    <input type="radio" id="deuteranopia" name="colorBlindness" value="deuteranopia"/>Deuteranopia
+                    <input type="radio" id="protanopia" name="colorBlindness" value="protanopia"/>Protanopia
+                    <input type="radio" id="tritanopia" name="colorBlindness" value="tritanopia"/>Tritanopia
+                </label>
+            </form>
         </div>
     );
 }
