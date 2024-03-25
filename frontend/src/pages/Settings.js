a0d55a42e64b9512f423725b26ef9a6b00fd8559
Delaney H <steam.avocado@pm.me>
2024-03-25 18:22:19 -0230

    Removed 'login' from the navbar Right-justified the 'signed in as' text

--- frontend/src/pages/Settings.js
+++ frontend/src/pages/Settings.js
@@ -5,6 +5,11 @@ export const Settings = () => {
             <h1>Settings</h1>
             <p>Change your settings here</p>
             <p>Placeholder</p>
+
+            <footer>
+                <a target="_blank" href="https://icons8.com/icon/vGQkspHvmobZ/settings">Settings</a> icon by <a
+                target="_blank" href="https://icons8.com">Icons8</a>
+            </footer>
         </div>
     );
 }
