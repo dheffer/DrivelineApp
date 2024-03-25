b67acc978b168b44dc1b97216876a72c78f91641
Delaney H <steam.avocado@pm.me>
2024-03-25 00:06:26 -0230

    Vehicle cards added on garage page

--- frontend/src/pages/garage/Garage.js
+++ frontend/src/pages/garage/Garage.js
@@ -1,12 +1,15 @@
-import {BrowserRouter, Routes, Route, Link, useNavigate} from "react-router-dom";
+import {BrowserRouter, Routes, Route, Link, useNavigate, useParams} from "react-router-dom";
 import AddRemoveNavbar from "./AddRemoveNavbar";
 import VehicleInfo from "../vehicle/VehicleInfo";
 import Accordion from 'react-bootstrap/Accordion';
+import {Button, Card, Col, Row} from "react-bootstrap";
+import Container from 'react-bootstrap/Container';
 
 function Garage() {
     // TODO: REPLACE WITH REAL VALUES, THESE ARE USED AS TEST VALUES
     const vehicles = ["2009 Nord Campy", "2015 Yotota Bav", "2019 Pesla Godel3", "2014 Nord Bustang",
      "2017 Yotota Camry", "2016 Pesla GodelS", "2018 Nord Bustang", "2010 Yotota Bav"]
+    let length = 0;
   return (
       <div>
           <AddRemoveNavbar/>
@@ -16,50 +19,33 @@ function Garage() {
               <Route path={"/garage/vehicle-info/:vehicle"} element={<VehicleInfo />} />
           </Routes>
 
-          <Accordion defaultActiveKey="0">
-                <Accordion.Item eventKey="0">
-                    <Accordion.Header>Vehicle List</Accordion.Header>
-                    <Accordion.Body>
-                        <table>
-                            <tbody>
-                            {vehicles.map((vehicle,id) => {
-                                let param = vehicle.replaceAll(" ", "-").toLowerCase();
-                                return (
-                                    <tr key={id}>
-                                        <td><Link to={"/garage/vehicle-info/"+param} >{vehicle}</Link></td>
-                                    </tr>
-                                )
-                            })}
-                            </tbody>
-                        </table>
-                    </Accordion.Body>
-                </Accordion.Item>
-          </Accordion>
-
-          {vehicles.map((vehicle,id) => {
-              let param = vehicle.replaceAll(" ", "-").toLowerCase();
-              return (
-                  <Accordion.Item eventKey={id+""}>
-                      <Accordion.Header>{vehicle}</Accordion.Header>
-                      <Accordion.Body>
-                          <Link to={"/garage/vehicle-info/"+param} >Info</Link>
-                          <Link to={"/garage/vehicle-history/"+param} >History</Link>
-                      </Accordion.Body>
-                  </Accordion.Item>
-              )
-          })}
-          <table>
-          <tbody>
-          {vehicles.map((vehicle,id) => {
-              let param = vehicle.replaceAll(" ", "-").toLowerCase();
-              return (
-                  <tr key={id}>
-                      <td><Link to={"/garage/vehicle-info/"+param} >{vehicle}</Link></td>
-                  </tr>
-              )
-          })}
-          </tbody>
-          </table>
+          <Row xs={1} md={3} className="g-4">
+              {vehicles.map((vehicle,id) => {
+                  let param = vehicle.replaceAll(" ", "-").toLowerCase();
+                  let year = vehicle.split(" ")[0];
+                  let model = vehicle.split(" ")[1]
+                  let make = vehicle.split(" ")[2];
+                  model = model[0].toUpperCase() + model.slice(1);
+                  make = make[0].toUpperCase() + make.slice(1);
+                  if (length === 2) {
+                      length = 0
+                  }
+                  length++;
+                  return (
+                      <Card style={{ width: '18rem' }} id={id+""}>
+                          <Card.Img variant="top" src="https://cdn.dealerk.it/cars/placeholder/placeholder-800.png" />
+                          <Card.Body>
+                              <Card.Title>{vehicle}</Card.Title>
+                              <Card.Text>
+                                  Some quick example text to build on the card title and make up the
+                                  bulk of the card's content.
+                              </Card.Text>
+                              <Button variant="primary"><Link to={"/garage/vehicle-info/"+param} ></Link>View Info</Button>
+                          </Card.Body>
+                      </Card>
+                  )
+              })}
+          </Row>
       </div>
   );
 }
--- frontend/src/pages/vehicle/VehicleInfo.js
+++ frontend/src/pages/vehicle/VehicleInfo.js
@@ -1,5 +1,6 @@
 import {BrowserRouter, Routes, Route, useParams} from "react-router-dom";
 import VehicleNavbar from "./VehicleNavbar";
+import {Card, Col, Row} from "react-bootstrap";
 
 function VehicleInfo() {
     let { vehicle } = useParams();
@@ -24,3 +25,23 @@ function VehicleInfo() {
 }
 
 export default VehicleInfo;
+
+
+
+<Row xs={1} md={2} className="g-4">
+    {Array.from({ length: 4 }).map((_, idx) => (
+        <Col key={idx}>
+            <Card>
+                <Card.Img variant="top" src="holder.js/100px160" />
+                <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                        This is a longer card with supporting text below as a natural
+                        lead-in to additional content. This content is a little bit
+                        longer.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </Col>
+    ))}
+</Row>
