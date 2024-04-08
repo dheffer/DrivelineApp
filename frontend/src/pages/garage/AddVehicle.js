6e8a5456b1512eb4f5951d87957be7b31ea6d324
ntuff <ndtuff@gmail.com>
2024-04-08 01:02:07 -0230

    Fixed dropdown menus

--- frontend/src/pages/garage/AddVehicle.js
+++ frontend/src/pages/garage/AddVehicle.js
@@ -1,4 +1,7 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+// import { Button, Form } from 'react-bootstrap';
+import { Button, Form, Container, Row, Col } from 'react-bootstrap';
+import '../../App.css';
 
 function AddVehicle() {
     const [vehicleAdded, setVehicleAdded] = useState(false);
@@ -18,82 +21,9 @@ function AddVehicle() {
         transmission: ""
     });
 
-    const [currentDropdown, setCurrentDropdown] = useState('years');
-
     useEffect(() => {
         getDropdownValues('years');
     }, []);
-
-    const getDropdownValues = async (value) => {
-
-        const myHeaders = new Headers();
-        const reqOptions = {
-            method: 'GET',
-            headers: myHeaders,
-            redirect: 'follow'
-        };
-
-        await fetch(`/api/get-${value}`, reqOptions) // make the gets for specific values
-        .then(res => {
-            if (!res.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return res.json();
-        })
-        .then(data => {
-            const years = data.map(item => item[value]);
-            const allYears = data.reduce((acc, curr) => [...acc, ...curr.years], []);
-            setDropdownValues(prevDropdownValues => ({
-                ...prevDropdownValues,
-                [value]: allYears
-            }));
-        })
-        .catch(error => {
-            console.error('There has been a problem with your fetch operation:', error);
-        });
-    }
-
-    const selectVehicle = async () => {
-        try{
-            const response = await fetch(`/api/get-config-id?year=${selectedVehicle.year}&make=${selectedVehicle.make}&model=${selectedVehicle.model}&engine=${selectedVehicle.engine}&transmission=${selectedVehicle.transmission}`);
-            if(!response.ok) {
-                console.log("Error selecting vehicle");
-                return;
-            }
-            const data = await response.json();
-            console.log("CONFIG ID: " + data.config_id);
-
-            setConfigId(data.config_id);
-        }
-        catch(error) {
-            console.log(error);
-        }
-    }
-
-    const addVehicleToUser = async () => {
-        console.log("CONFIG ID in ADD vehicle: " + configId);
-        try{
-            const response = await fetch('/api/add-vehicle', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    email: "johnson@gmail,com",
-                    config_id: configId
-                })
-            });
-            if(!response.ok) {
-                console.log("Error adding vehicle to user");
-                return;
-            }
-            setVehicleAdded(true);
-    }
-    catch(error) {
-        console.log(error);
-    }
-}
-
 
     const handleChange = async (e) => {
         const { name, value } = e.target;
@@ -101,163 +31,169 @@ function AddVehicle() {
             ...prevSelectedVehicle,
             [name]: value
         }));
-        console.log("NAME: " + name)
-        console.log("VALUE: " + value)
-        console.log("Selected YEAR: " + selectedVehicle.year)
-        switch(name) {
-            case 'year':
-                setCurrentDropdown('makes');
-                setDropdownValues(prevDropdownValues => ({
-                    ...prevDropdownValues,
-                    makes: [],
-                    models: [],
-                    engines: [],
-                    transmissions: []
-                }));
-                const makesResponse = await fetch(`/api/get-makes?year=${value}`);
-                if(!makesResponse.ok) {
-                    console.log("Error getting makes");
-                    return;
-                }
-                const makesData = await makesResponse.json();
-                console.log("MAKES CHANGE: " + makesData)
-                setDropdownValues(prevDropdownValues => ({
-                    ...prevDropdownValues,
-                    makes: makesData
-                }));
-                setSelectedVehicle(prevSelectedVehicle => ({
-                    ...prevSelectedVehicle,
-                    [name]: value
-                }));
-                break;
-            case 'make':
-                setCurrentDropdown('models');
-                setDropdownValues(prevDropdownValues => ({
-                    ...prevDropdownValues,
-                    models: [],
-                    engines: [],
-                    transmissions: []
-                }));
-                const modelsResponse = await fetch(`/api/get-models?year=${selectedVehicle.year}&make=${value}`);
-                if(!modelsResponse.ok) {
-                    console.log("Error getting models");
-                    return;
-                }
-                const modelsData = await modelsResponse.json();
-                // const makes = makesData.map(item => item.make);
-                console.log("MODELS CHANGE: " + modelsData)
-                setSelectedVehicle(prevSelectedVehicle => ({
-                    ...prevSelectedVehicle,
-                    [name]: value
-                }));
-                console.log("Selected make: " + name + " " + value)
-                setDropdownValues(prevDropdownValues => ({
-                    ...prevDropdownValues,
-                    models: modelsData
-                }));
-                setSelectedVehicle(prevSelectedVehicle => ({
-                    ...prevSelectedVehicle,
-                    [name]: value
-                }));
-                break;
-            case 'model':
-                setCurrentDropdown('engines');
-                setDropdownValues(prevDropdownValues => ({
-                    ...prevDropdownValues,
-                    engines: [],
-                    transmissions: []
-                }));
-                const enginesResponse = await fetch(`/api/get-engines?year=${selectedVehicle.year}&make=${selectedVehicle.make}&model=${value}`);
-                if(!enginesResponse.ok) {
-                    console.log("Error getting Engines");
-                    return;
-                }
-                const enginesData = await enginesResponse.json();
-                console.log("ENGINES CHANGE: " + enginesData)
-                setDropdownValues(prevDropdownValues => ({
-                    ...prevDropdownValues,
-                    engines: enginesData
-                }))
-                setSelectedVehicle(prevSelectedVehicle => ({
-                    ...prevSelectedVehicle,
-                    [name]: value
-                }));
-                break;
-            case 'engine':
-                setCurrentDropdown('transmissions');
-                setDropdownValues(prevDropdownValues => ({
-                    ...prevDropdownValues,
-                    transmissions: []
-                }));
-                const transmissionsResponse = await fetch(`/api/get-transmissions?year=${selectedVehicle.year}&make=${selectedVehicle.make}&model=${selectedVehicle.model}&engine=${value}`);
-                if(!transmissionsResponse.ok) {
-                    console.log("Error getting Transmissions");
-                    return;
-                }
-                const transmissionsData = await transmissionsResponse.json();
-                console.log("TRANSMISSIONS CHANGE: " + transmissionsData)
-                setDropdownValues(prevDropdownValues => ({
-                    ...prevDropdownValues,
-                    transmissions: transmissionsData
-                }))
-                setSelectedVehicle(prevSelectedVehicle => ({
-                    ...prevSelectedVehicle,
-                    [name]: value
-                }));
-                break;
-            default:
-                break;
+
+        if (name === 'year') {
+            getDropdownValues('makes', value);
+            setDropdownValues(prev => ({ ...prev, models: [], engines: [], transmissions: [] }));
+        } else if (name === 'make') {
+            getDropdownValues('models', selectedVehicle.year, value);
+            setDropdownValues(prev => ({ ...prev, engines: [], transmissions: [] }));
+        } else if (name === 'model') {
+            getDropdownValues('engines', selectedVehicle.year, selectedVehicle.make, value);
+            setDropdownValues(prev => ({ ...prev, transmissions: [] }));
+        } else if (name === 'engine') {
+            getDropdownValues('transmissions', selectedVehicle.year, selectedVehicle.make, selectedVehicle.model, value);
         }
-    }
+    };
+
+    const getDropdownValues = async (category, year = '', make = '', model = '', engine = '') => {
+        let url = `/api/get-${category}?`;
+        const params = new URLSearchParams();
+
+        if (year) params.append('year', year);
+        if (make) params.append('make', make);
+        if (model) params.append('model', model);
+        if (engine && category === 'transmissions') params.append('engine', engine);
+
+        url += params.toString();
+
+        try {
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            let data = await response.json();
+
+            if (category === 'years') {
+                data = data.map(item => item._id);
+            }
+
+            setDropdownValues(prev => ({
+                ...prev,
+                [category]: data
+            }));
+        } catch (error) {
+            console.error(`Failed to fetch ${category}:`, error);
+        }
+    };
+
+    //TODO: Combine the following two functions into one
+    const selectVehicle = async () => {
+        const queryParameters = new URLSearchParams(selectedVehicle).toString();
+        try {
+            const response = await fetch(`/api/get-config-id?${queryParameters}`);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const data = await response.json();
+            if (data.config_id) {
+                setConfigId(data.config_id);
+            } else {
+                console.log("No config ID found for the selected vehicle.");
+            }
+        } catch (error) {
+            console.error("Error selecting vehicle:", error);
+        }
+    };
+
+    const addVehicleToUser = async () => {
+        try {
+            const response = await fetch('/api/add-vehicle', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: "user@example.com", // Replace with dynamic user email
+                    config_id: configId
+                })
+            });
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            setVehicleAdded(true);
+        } catch (error) {
+            console.error("Error adding vehicle to user:", error);
+        }
+    };
 
     return (
-        <div>
-            {currentDropdown === 'years' && (
-                <select name="year" value={selectedVehicle.year} onChange={handleChange}>
-                <option value="">Select Year</option>
-                {dropdownValues.years.map((year, index) => {
-                    return <option key={index} value={year}>{year}</option>
-                })}
-            </select>
-            )}
-            {currentDropdown === 'makes' && (
-                <select name="make" value={selectedVehicle.make} onChange={handleChange}>
-                <option value="">Select Make</option>
-                {dropdownValues.makes.map((make, index) => {
-                    return <option key={index} value={make}>{make}</option>
-                })}
-            </select>
-            )}
-            {currentDropdown === 'models' && (
-                <select name="model" value={selectedVehicle.model} onChange={handleChange}>
-                <option value="">Select Model</option>
-                {dropdownValues.models.map((model, index) => {
-                    return <option key={index} value={model}>{model}</option>
-                })}
-            </select>
-            )}
-            {currentDropdown === 'engines' && (
-                <select name="engine" value={selectedVehicle.engine} onChange={handleChange}>
-                <option value="">Select Engine</option>
-                {dropdownValues.engines.map((engine, index) => {
-                    return <option key={index} value={engine}>{engine}</option>
-                })}
-            </select>
-            )}
-            {currentDropdown === 'transmissions' && (
-                <select name="transmission" value={selectedVehicle.transmission} onChange={handleChange}>
-                <option value="">Select Transmission</option>
-                {dropdownValues.transmissions.map((transmission, index) => {
-                    return <option key={index} value={transmission}>{transmission}</option>
-                })}
-            </select>
-            )}
-            <button onClick={selectVehicle}>Select Vehicle</button>
-            {configId && <button onClick={addVehicleToUser}>Add Vehicle</button>}
-            {vehicleAdded && <p>Vehicle has been added successfully!</p>}
-        </div>
-    )
-}
+        <Container>
+            <Row className="justify-content-md-center">
+                <Col md={6}> {/* This sets the max width of the column to half the parent width */}
+                    <Form>
+                        <Form.Group controlId="yearSelect">
+                            <Form.Label>Year</Form.Label>
+                            <Form.Select name="year" value={selectedVehicle.year} onChange={handleChange}>
+                                <option value="">Select Year</option>
+                                {dropdownValues.years.map((year, index) => (
+                                    <option key={index} value={year}>{year}</option>
+                                ))}
+                            </Form.Select>
+                        </Form.Group>
 
+                        {selectedVehicle.year && (
+                            <Form.Group controlId="makeSelect">
+                                <Form.Label>Make</Form.Label>
+                                <Form.Select name="make" value={selectedVehicle.make} onChange={handleChange}>
+                                    <option value="">Select Make</option>
+                                    {dropdownValues.makes.map((make, index) => (
+                                        <option key={index} value={make}>{make}</option>
+                                    ))}
+                                </Form.Select>
+                            </Form.Group>
+                        )}
+
+                        {selectedVehicle.make && (
+                            <Form.Group controlId="modelSelect">
+                                <Form.Label>Model</Form.Label>
+                                <Form.Select name="model" value={selectedVehicle.model} onChange={handleChange}>
+                                    <option value="">Select Model</option>
+                                    {dropdownValues.models.map((model, index) => (
+                                        <option key={index} value={model}>{model}</option>
+                                    ))}
+                                </Form.Select>
+                            </Form.Group>
+                        )}
+
+                        {selectedVehicle.model && (
+                            <Form.Group controlId="engineSelect">
+                                <Form.Label>Engine</Form.Label>
+                                <Form.Select name="engine" value={selectedVehicle.engine} onChange={handleChange}>
+                                    <option value="">Select Engine</option>
+                                    {dropdownValues.engines.map((engine, index) => (
+                                        <option key={index} value={engine}>{engine}</option>
+                                    ))}
+                                </Form.Select>
+                            </Form.Group>
+                        )}
+
+                        {selectedVehicle.engine && (
+                            <Form.Group controlId="transmissionSelect">
+                                <Form.Label>Transmission</Form.Label>
+                                <Form.Select name="transmission" value={selectedVehicle.transmission} onChange={handleChange}>
+                                    <option value="">Select Transmission</option>
+                                    {dropdownValues.transmissions.map((transmission, index) => (
+                                        <option key={index} value={transmission}>{transmission}</option>
+                                    ))}
+                                </Form.Select>
+                            </Form.Group>
+                        )}
+
+                        <div className="text-center mt-4"> {/* Center the buttons and add some top margin */}
+                            <Button className="custom-button-primary mx-2" onClick={selectVehicle} disabled={!selectedVehicle.transmission}>Select Vehicle</Button>
+                            <Button className="custom-button-success mx-2" onClick={addVehicleToUser} disabled={!configId}>Add Vehicle</Button>
+                        </div>
+                        {vehicleAdded && <div className="alert alert-success mt-4" role="alert">
+                            Vehicle has been added successfully!
+                        </div>}
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
+    );
+
+}
 
 export default AddVehicle;
